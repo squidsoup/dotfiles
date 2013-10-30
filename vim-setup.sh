c764d53542ed3b882febec848513d0d156e87c94
Kit Randel <kit@nocturne.net.nz>
2013-10-30 15:24:56 +1300

    run BundleInstall from script

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ln -s .vim ~/.vim
-ln -s .vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/
+ln -s ~/dotfiles/.vimrc ~/
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +BundleInstall +qall
