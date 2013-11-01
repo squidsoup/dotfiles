dee65de5b73727a570191a6bd53b6af7f2457d31
Kit Randel <kit@nocturne.net.nz>
2013-11-01 16:48:23 +1300

    added simple setup script for bootstrapping new shell on linux/osx

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+os=${OSTYPE//[0-9.]/}
+# bootstrap new shell
+if [[ os == 'linux-gnu' ]]; then
+  sudo apt-get install zsh
+elif [[ os == 'darwin' ]]; then
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  brew install zsh
+fi
+chsh -s /bin/zsh $USER
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+ln -fs .zshrc ~/
+ln -fs .zshenv ~/
+
