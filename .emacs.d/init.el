23681bc2e2e860ce6cdff47c036bf28ef4b5e64d
Kit Randel <kit@nocturne.net.nz>
2014-01-17 15:39:31 +1300

    emacs setup

---
+++ .emacs.d/init.el
@@ -0,0 +1,30 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Packages
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-ruby
+                      clojure-mode
+                      clojure-test-mode
+                      nrepl
+                      jedi
+                      flycheck
+                      markdown-mode
+                      js2-mode
+                      )
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(load "~/.emacs.d/user.el")
---
+++ .emacs.d/user.el
@@ -0,0 +1,124 @@
+;; This is where your customizations should live
+
+;; env PATH
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+;; Uncomment the lines below by removing semicolons and play with the
+;; values in order to set the width (in characters wide) and height
+;; (in lines high) Emacs will have whenever you start it
+
+;;(setq initial-frame-alist '((top . 0) (left . 0) (width . 80) (height . 20)))
+
+
+;; Place downloaded elisp files in this directory. You'll then be able
+;; to load them.
+;;
+;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
+;; then you can add the following code to this file:
+;;
+;; (require 'yaml-mode)
+;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+;; 
+;; Adding this code will make Emacs enter yaml mode whenever you open
+;; a .yml file
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; Themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+;; Uncomment this to increase font size
+;; (set-face-attribute 'default nil :height 140)
+;; This is where your customizations should live
+
+;; env PATH
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+;; Uncomment the lines below by removing semicolons and play with the
+;; values in order to set the width (in characters wide) and height
+;; (in lines high) Emacs will have whenever you start it
+
+;;(setq initial-frame-alist '((top . 0) (left . 0) (width . 80) (height . 20)))
+
+
+;; Place downloaded elisp files in this directory. You'll then be able
+;; to load them.
+;;
+;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
+;; then you can add the following code to this file:
+;;
+;; (require 'yaml-mode)
+;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+;; 
+;; Adding this code will make Emacs enter yaml mode whenever you open
+;; a .yml file
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; Themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+;; Uncomment this to increase font size
+;;(set-face-attribute 'default nil :height 140)
+(load-theme 'tomorrow-night-bright t)
+;;(load-theme 'andreas t)
+
+;; Flyspell often slows down editing so it's turned off
+(remove-hook 'text-mode-hook 'turn-on-flyspell)
+
+;; ansi-term
+(setq system-uses-terminfo nil) ;; fixes weird 4m characters
+;; JavaScript
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(setq js2-missing-semi-one-line-override t)
+(setq-default js2-basic-offset 2)
+
+;; Clojure
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
+(setq nrepl-history-file "~/.emacs.d/nrepl-history")
+(setq nrepl-popup-stacktraces t)
+(setq nrepl-popup-stacktraces-in-repl t)
+(add-hook 'nrepl-connected-hook
+          (defun pnh-clojure-mode-eldoc-hook ()
+            (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
+            (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+            (nrepl-enable-on-existing-clojure-buffers)))
+(add-hook 'nrepl-mode-hook 'subword-mode)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'clojure-mode-hook 'whitespace-mode)
+
+;; Python
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+(add-hook 'python-mode-hook 'whitespace-mode)
+
+;; Don't edit python bytecode
+(add-to-list 'completion-ignored-extensions ".pyc")
+
+;; Markdown
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+;; hippie expand - don't try to complete with file names
+(setq hippie-expand-try-functions-list (delete 'try-complete-file-name hippie-expand-try-functions-list))
+(setq hippie-expand-try-functions-list (delete 'try-complete-file-name-partially hippie-expand-try-functions-list))
+
+(setq ido-use-filename-at-point nil)
+
+;; Save here instead of littering current directory with emacs backup files
+(setq backup-directory-alist `(("." . "~/.saves")))
