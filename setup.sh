c380510e28653f59897dbd94b128b17062ee835a
Kit Randel <kit@nocturne.net.nz>
2014-02-21 13:48:12 +1300

    link emacs.d on setup

--- setup.sh
+++ setup.sh
@@ -13,4 +13,4 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 ln -fs $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -fs $HOME/dotfiles/.zshenv $HOME/.zshenv
-
+ln -fs $HOME/dotfiles/.emacs.d $HOME/.emacs.d
