62a1ed507a79634271bf0f8e6b2787787b221c98
Kit Randel <kit@nocturne.net.nz>
2014-07-14 22:06:55 +0000

    Improved setup script, and made .zshenv generic (copied rather than simlinked now)

--- .zshenv
+++ .zshenv
@@ -1,25 +1,13 @@
+# Some sensible defaults
 export os=${OSTYPE//[0-9.]/}
 export EDITOR='emacs'
-export ANDROID_HOME="$HOME/SDK/android-sdk-macosx"
 export TODO=~/Dropbox/todo
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_13.jdk/Contents/Home"
-export JDK_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_13.jdk/Contents/Home"
-export AWS_IAM_HOME="$HOME/aws-dev/IAMCli-1.5.0"
-export AWS_CREDENTIAL_FILE=$HOME/.elasticbeanstalk/aws_credential_file
-export CLASSPATH="$CLASSPATH:$HOME/.lein/self-installs/leiningen-1.7.1-standalone.jar"
-export GOPATH="~/Dropbox/Projects/go"
-export GOROOT="/usr/local/Cellar/go/1.0.3"
+
 # PATH
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/usr/X11/bin:$HOME/bin
 export PATH=$PATH:/usr/local/lib/node_modules # npm
 export PATH=$PATH:$HOME/SDK/jdk/bin # JDK 
-export PATH=$PATH:$HOME/aws-dev/eb/macosx/python2.7 # Elastic Beanstalk tool
 export PATH=$PATH:$HOME/.cabal/bin # Haskell binaries 
-export PATH=$PATH:$HOME/aws-dev/IAMCli-1.5.0/bin 
-export PATH=$PATH:/usr/local/texlive/bin # MacTex/TexLive
-export PATH=$PATH:/usr/local/amazon/kindlegen
-export PATH=$PATH:/Applications/Racket\ v5.3.5/bin
-export PATH=$PATH:/usr/local/munki
 
 export WORKON_HOME=~/env
 # active venv in prompt managed by oh-my-zsh theme
--- setup.sh
+++ setup.sh
@@ -1,16 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-os=${OSTYPE//[0-9.]/}
 # bootstrap new shell
-if [[ os == 'linux-gnu' ]]; then
-  sudo apt-get install zsh
-elif [[ os == 'darwin' ]]; then
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-  brew install zsh
-fi
-chsh -s /bin/zsh $USER
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+echo 'Detecting OS and checking for zsh...'
+os=${OSTYPE//[0-9.]/}
 
+
+if ! hash zsh 2>/dev/null; then
+    echo 'Installing zsh...'
+    if [[ os == 'linux-gnu' ]]; then
+        sudo apt-get install zsh
+    elif [[ os == 'darwin' ]]; then
+        if ! hash brew 2>/dev/null; then
+            ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+        fi
+        brew install zsh
+    fi
+fi
+
+echo 'Setting zsh to default shell...'
+chsh -s /bin/zsh $USER
+
+if ! [[ -d $HOME/.oh-my-zsh ]]; then
+    echo 'Installing Oh-My-Zsh!'
+    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+echo 'Setting up zshrc defaults'
 ln -fs $HOME/dotfiles/.zshrc $HOME/.zshrc
-ln -fs $HOME/dotfiles/.zshenv $HOME/.zshenv
+if ! [[ -f $HOME/.zshenv ]]; then
+    cp $HOME/dotfiles/.zshenv $HOME/.zshenv
+fi
+echo 'Setting up emacs defaults'
 ln -fs $HOME/dotfiles/.emacs.d $HOME/.emacs.d
+
+echo 'Done.'
