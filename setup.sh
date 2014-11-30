5e6655b690a7b31104e9de888a7eb9fa44a68b99
Kit Randel <kit@nocturne.net.nz>
2014-11-30 14:38:16 +1300

    git: added aliases

--- setup.sh
+++ setup.sh
@@ -39,6 +39,10 @@ echo 'Setting up git default config...'
 git config --global user.email "kit@nocturne.net.nz"
 git config --global user.name "Kit Randel" 
 git config --global push.default simple
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
 
 echo 'Switching shell...'
 zsh
