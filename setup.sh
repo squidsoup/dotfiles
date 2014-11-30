87ee94fcc4257d710a6404cb468b22302ddb6186
Kit Randel <kit@nocturne.net.nz>
2014-11-30 15:24:37 +1300

    more git aliases

--- setup.sh
+++ setup.sh
@@ -43,6 +43,8 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.unstage 'reset HEAD --'
 
 echo 'Switching shell...'
 zsh
