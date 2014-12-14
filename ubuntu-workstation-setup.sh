1ca586d32aa30144572066d79ca2e331b40273b4
Kit Randel <kit@nocturne.net.nz>
2014-12-15 10:07:58 +1300

    ws setup: juju-local

--- ubuntu-workstation-setup.sh
+++ ubuntu-workstation-setup.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository ppa:juju/stable
 sudo apt-get update
 
 # packages
-sudo apt-get install -y --no-install-recommends emacs keepassx gnome-tweak-tool chromium-browser tmux vlc virtualbox lxc packaging-dev irssi xclip virtualenv virtualenvwrapper libimobiledevice-utils bzr bzr-pqm htop libssl-dev tree juju-core
+sudo apt-get install -y --no-install-recommends emacs keepassx gnome-tweak-tool chromium-browser tmux vlc virtualbox lxc packaging-dev irssi xclip virtualenv virtualenvwrapper libimobiledevice-utils bzr bzr-pqm htop libssl-dev tree juju-core juju-local
 
 # rbenv
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
