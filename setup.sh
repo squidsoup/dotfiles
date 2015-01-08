e81173902d850106f5c541ef96d0f048770584fc
Kit Randel <kit@nocturne.net.nz>
2015-01-08 15:42:56 +1300

    git: enable rerere

--- setup.sh
+++ setup.sh
@@ -45,6 +45,7 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.unstage 'reset HEAD --'
+git config --global rerere.enabled
 
 echo 'Switching shell...'
 zsh
